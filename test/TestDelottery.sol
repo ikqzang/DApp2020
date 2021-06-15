7b63133be2695d9df4e5dc88bd00c33fa3c9fbf9
ikqzang <60028979+ikqzang@users.noreply.github.com>
2021-06-15 13:51:36 +0700

    Update TestDelottery.sol

--- test/TestDelottery.sol
+++ test/TestDelottery.sol
@@ -7,21 +7,22 @@ import "../contracts/Delottery.sol";
 contract TestDelottery {
   Delottery delotto = Delottery(DeployedAddresses.Delottery());
 
-  uint expectedId = 9;
-  uint expectedprice = 8;
-  bool expectedresult = true;
-  uint expectedBuyCount = 8;
+  uint expectedLottoNum = 1;
+  uint expectedAmount = 100;
+  bool expectedResult = true;
+  address expectedId = msg.sender;
+  
 
-  function testbuyLotteryByID() public {
-    (uint returnId,uint returnPrice,bool returnresult)  = delotto.buyLotto(expectedId,expectedprice);
-    Assert.equal(returnId, expectedId, "buyLotteryByID of the expected id should match what is returned.");
-    Assert.equal(returnPrice, expectedprice, "buyLotteryByID of the expected price should match what is returned.");
-    Assert.equal(returnresult, expectedresult, "buyLotteryByID of the expected result should match what is returned.");
+  function testbookLottoByID() public  {
+    (bool returnResult)  = delotto.bookLotto(expectedId,expectedLottoNum,expectedAmount);
+    Assert.equal(returnResult, expectedResult, "buyLotteryByID of the expected result should match what is returned.");
   }
   
-  function testgetBuyCountById() public {
-    uint returnLotto = delotto.getBuyCount(expectedId) ;
-    Assert.equal(returnLotto, expectedBuyCount, "getBuyCountById of the expected result should match what is returned.");
+  
+  function testgetBookingById() public {
+    (uint returnLottoNum,uint returnAmount) = delotto.getBooking(expectedId);
+    Assert.equal(returnLottoNum, expectedLottoNum, "getBookingById of the expected LottoNum should match what is returned.");
+    Assert.equal(returnAmount, expectedAmount, "getBookingById of the expected Amount should match what is returned.");
   }
   
 }
