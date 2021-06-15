fc6179e9c9b71fe5c7be091b6e136394626d8ca1
ikqzang <60028979+ikqzang@users.noreply.github.com>
2021-06-15 13:49:22 +0700

    Update Delottery.sol

--- contracts/Delottery.sol
+++ contracts/Delottery.sol
@@ -2,41 +2,25 @@ pragma solidity >=0.5.0;
 
 contract Delottery {
     struct player {
-        address playerId;
         uint lottoNum;
         uint amount;
     } 
     mapping (address => player) players;
 
-    function bookLotto(uint _lottoNum,uint _amount) public returns (address,bool){            
-        bool result = false;                                                          
-        address _user = msg.sender;                                                   
-        players[_user] = player({                                                     
-        playerId : _user,                                                             
+    function bookLotto(address playerAdress,uint _lottoNum,uint _amount) public returns (bool){            
+        bool result = false;                                                                                                           
+        players[playerAdress] = player
+        ({                                                                                                                  
         lottoNum : _lottoNum,                                                         
         amount : _amount                                                              
-        });                                                                           
-        result = true;                                                                
-        return (_user,result);                                                              
+        });
+
+        result = true;
+
+        return (result);                                                              
     }
 
-    function getBooking(address _playerid,uint _lottoNum,uint _amount) public view returns (uint) {
-        uint result = 0;   
-         
-        player memory booking = players[_playerid];
-        if (booking.playerId == _playerid){
-            result += 1;
-        } 
-        if (booking.lottoNum == _lottoNum){
-            result += 1;
-        } 
-        if (booking.amount == _amount){
-            result += 1;
-        } 
-        
-        return (result);
+    function getBooking(address playerAddress) public view returns (uint,uint) {    
+        player memory booking = players[playerAddress];
+        return (booking.lottoNum,booking.amount);
     }
-
-
-}
-    
